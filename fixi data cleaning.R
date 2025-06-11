96caeafb4a78a61d5d587d31b9c9428200eaeebc
JayLo178 <jurvdloo@hotmail.com>
2025-06-11 16:08:17 +0200

    Aangepast om wat meer inzicht in de data te krijgen, zoals veel voorkomende woorden per categorie.

--- fixi data cleaning.R
+++ fixi data cleaning.R
@@ -3,17 +3,25 @@ library(tidyverse)
 library(DBI)
 library(odbc)
 
-# Laad brondata in thuis/werk
-pc <- "werk"
+# Alvast paden definiëren
+raw_data_output_path <- "raw_data/fixi_data.RDS"
+raw_data_tokenized_output_path <- "raw_data/fixi_data_tokenized.RDS"
+metadata_output_path <- "clean_data/fixi_metadata_clean.RDS"
+tekst_output_path <- "clean_data/fixi_tekst_clean.RDS"
 
-if (pc == "thuis") {
-  # .csv bestand
-  path <- "C:/Users/jurvd/OneDrive/Bureaublad/fixi_meldingen_20250611.csv"  
-  cols <- c("id", "categoryName", "teamName", "isAnonymous", "source", "created",
-            "regionName", "address", "latitude", "longitude", "description")
-  data <- read.csv(path, sep = ";", header = FALSE, stringsAsFactors = FALSE,
-                   col.names = cols)
-} else if (pc == "werk") {
+# Laad brondata in nieuw/bestaand 
+# .csv bestand
+#path <- "C:/Users/jurvd/OneDrive/Bureaublad/fixi_meldingen_20250611.csv"  
+#cols <- c("id", "categoryName", "teamName", "isAnonymous", "source", "created",
+#          "regionName", "address", "latitude", "longitude", "description")
+#data <- read.csv(path, sep = ";", header = FALSE, stringsAsFactors = FALSE,
+#                 col.names = cols)
+
+versie <- "bestaand"
+
+if (versie == "bestaand") {
+  data <- readRDS("raw_data/fixi_data.RDS")
+} else if (versie == "nieuw") {
   sql_con <- dbConnect(odbc(),
                        driver = "ODBC Driver 17 for SQL Server",
                        server = "sql-dataplatform.database.windows.net",
@@ -40,11 +48,10 @@ if (pc == "thuis") {
     	AND DATEDIFF(dd, created, GETDATE()) <= 365")
   
   dbDisconnect(sql_con)
+  
+  # Ruwe data opslaan zodat niet steeds de verbinding hoeft te worden gemaakt bij testen
+  saveRDS(data, raw_data_output_path)
 }
-
-# Alvast pad definiëren
-metadata_output_path <- "clean_data/fixi_metadata_clean.RDS"
-tekst_output_path <- "clean_data/fixi_tekst_clean.RDS"
 
 # Bekijk welke categorieën er zijn
 data |> 
@@ -79,11 +86,20 @@ tekst_verwerkt <- data |>
 
 View(tekst_verwerkt$token)
 
+# Sla op 
+saveRDS(tekst_verwerkt$token, raw_data_tokenized_output_path)
+# Lees in
+tekst_verwerkt <- readRDS(raw_data_tokenized_output_path)
+
+# Alles naar lower case
+tekst_verwerkt <- tekst_verwerkt |> 
+  mutate(lemma = tolower(lemma))
+
 # Filter bepaalde woordcategorieen eruit
 # ADP (bindwoorden zoals voorzetsels), PUNCT (interpunctietekens), NUM (cijfers), SYM (tekens zoals € %), 
 # AUX (hulpwerkwoorden zoals zijn, hebben), PRON (voornaamwoorden zoals ik, jij, hij), DET (lidwoorden en aanwijzende/ bezittelijke voornaamwoorden zoals de, dit)
 # CCONJ (nevenschikkende voegwoorden zoals en, maar) 
-tekst_verwerkt <- tekst_verwerkt$token |> 
+tekst_verwerkt <- tekst_verwerkt |> 
   filter(!upos %in% c("ADP", "PUNCT", "NUM", "SYM", "AUX", "PRON", "DET", "CCONJ", "X"))
 
 # Verwijder ook de stopwoorden 
