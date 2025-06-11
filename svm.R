81ae623487f78e8406651af4484732a6d0143343
JayLo178 <jurvdloo@hotmail.com>
2025-06-11 16:07:40 +0200

    Tuning grid toegevoegd en generieke categorie weggelaten. Nog steeds amper verbetering.

--- svm.R
+++ svm.R
@@ -3,15 +3,29 @@ library(tidyverse)
 library(tidymodels)
 library(tidytext)
 
-# Laad data (tokenized dataset van de mor meldingen)
-# mor_data <- readRDS(...)
+# Laad data (tokenized dataset van de meldingen) en metadata (kenmerken van meldingen)
 fixi_data <- readRDS("clean_data/fixi_tekst_clean.rds")
 fixi_metadata <- readRDS("clean_data/fixi_metadata_clean.rds")
+
+# Voeg categorie toe aan fixi_data
+fixi_data <- fixi_data |> 
+  left_join(fixi_metadata |> select(id, categoryName), by = join_by(doc_id == id))
+
+# Laat categorie OVerlast en handhaving weg -> te generiek voor dit model
+fixi_data <- fixi_data |> 
+  filter(categoryName != "Overlast en handhaving")
 
 # Tf-idf score berekenen van de meest voorkomende woorden
 top_words <- fixi_data |> 
   count(lemma, sort = TRUE) |> 
   slice_max(n, n = 750)
+
+# 10 meest voorkomende woorden per categorie
+top_words_per_cat <- fixi_data |> 
+  count(categoryName, lemma, sort = TRUE) |>
+  group_by(categoryName) |> 
+  slice_max(n, n = 10) |> 
+  ungroup()
 
 word_freq <- fixi_data |> 
   count(doc_id, lemma, sort = TRUE) %>%
@@ -23,7 +37,7 @@ doc_vectors <- word_freq |>
 
 # Voeg categorieën toe aan de document vectoren
 doc_vectors <- doc_vectors |> 
-  left_join(fixi_metadata |> select(doc_id, categoryName), by = "doc_id")
+  left_join(fixi_metadata |> select(id, categoryName), by = join_by(doc_id == id))
 
 # Maak een factor van de categoryName kolom
 doc_vectors <- doc_vectors |> 
@@ -32,6 +46,7 @@ doc_vectors <- doc_vectors |>
 doc_vectors |> 
   count(categoryName)
 
+# Misschien nog SMOTE om verdeling rechter te trekken??
 
 
 # Splits data in training en test set
@@ -39,7 +54,7 @@ set.seed(42)
 
 # Voor eerste poging maar 10% van de data gebruiken
 doc_vectors <- doc_vectors |> 
-  sample_frac(0.1)
+  sample_frac(0.3)
 
 data_split <- initial_split(doc_vectors, prop = 0.8, strata = categoryName)
 
@@ -50,6 +65,8 @@ test_data <- testing(data_split)
 recipe_spec <- recipe(categoryName ~ ., data = train_data) |> 
   # Doc id wordt niet gebruikt in de model training
   update_role(doc_id, new_role = "ID") |> 
+  # Verwijder kolommen met variantie = 0
+  step_zv(all_predictors()) |> 
   # Normaliseer de numerieke waarden
   step_normalize(all_predictors())
 
@@ -65,8 +82,8 @@ baked_test <- recipe_spec |>
 # SVM model initialiseren
 svm_spec <- svm_rbf(
   mode = "classification",
-  cost = 10,
-  rbf_sigma = 0.1
+  cost = tune(),
+  rbf_sigma = tune()
 ) |> 
   set_engine("kernlab")
 
@@ -74,6 +91,27 @@ svm_spec <- svm_rbf(
 fixi_wf <- workflow() |> 
   add_recipe(recipe_spec) |> 
   add_model(svm_spec)
+
+# Maak een grid van waarden om mee te trainen
+grid <- expand.grid(
+  cost = c(1, 10, 100),
+  rbf_sigma = c(0.01, 0.1, 1)
+)
+
+# Train het model met de grid search
+model <- tune_grid(
+  fixi_wf,
+  resamples = vfold_cv(train_data, v = 5),
+  grid = grid
+)
+
+best_params <- select_best(model, metric = "accuracy")
+
+# Pas de beste parameters toe op het model
+fixi_wf <- finalize_workflow(
+  fixi_wf,
+  best_params
+)
 
 # Train het model
 fixi_fit <- fixi_wf |> 
