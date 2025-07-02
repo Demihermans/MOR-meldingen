6604f0944aad8c3e02f1a281f37b3f4fd6c54079
JayLo178 <jurvdloo@hotmail.com>
2025-07-02 14:52:36 +0200

    Wat plotjes voor beschrijvende statistiek en verkenning van dataset

--- fixi eda.R
+++ fixi eda.R
@@ -1,17 +1,55 @@
-# Packages
 library(tidyverse)
+library(ggplot2)
 
-# Laad bronbestand in
-path <- "C:/Users/jurvd/OneDrive/Bureaublad/fixi_meldingen_20250521.csv"
-cols <- c("teamName", "subCatName", "source", "regionName", "address", "addressDetails",
-          "categoryName", "created", "closed", "description", "latitude", "longitude", "status")
+data <- readRDS("clean_data/fixi_clean.RDS")
 
-data <- read.csv(path, sep = ";", header = FALSE, stringsAsFactors = FALSE, col.names = cols)
-
-# Aantal lege rijen van elke kolom
-empty_rows <- sapply(data, function(x) sum(x == ""))
-empty_rows
-
-# Verwijder subCatName en addressDetails
+# Maak datum type van created
 data <- data |> 
-  select(-subCatName, -addressDetails, -closed)
+  mutate(created = as.Date(created, format = "%Y-%m-%d"))
+
+# Voeg kolommen jaar, kwartaal, maand toe aan data
+data <- data |> 
+  mutate(
+    jaar = format(created, "%Y"),
+    kwartaal = quarters(created),
+    maand = format(created, '%m'),
+    jaarkwartaal = paste(jaar, kwartaal, sep = " "),
+    jaarmaand = paste(jaar, maand, sep = " ")
+  )
+
+data <- data |> 
+  filter(jaar > 2018)
+
+# Plot het aantal meldingen per jaar
+ggplot(data, aes(x = jaar)) +
+  geom_bar() +
+  labs(title = "Aantal meldingen per jaar", x = "Jaar", y = "Aantal meldingen") +
+  theme_minimal() +
+  scale_fill_brewer(palette = "Set3") +
+  theme(legend.position = "none")
+
+# Aantal meldingen per jaar en kwartaal
+ggplot(data, aes(x = jaarkwartaal)) +
+  geom_bar() +
+  labs(title = "Aantal meldingen per kwartaal", x = "Jaar - kwartaal", y = "Aantal meldingen") +
+  theme_minimal() +
+  scale_fill_brewer(palette = "Set3") +
+  theme(legend.position = "none")
+
+# Aantal meldingen per jaar en maand
+ggplot(data, aes(x = jaarmaand)) +
+  geom_bar() +
+  labs(title = "Aantal meldingen per maand", x = "Jaar - maand", y = "Aantal meldingen") +
+  theme_minimal() +
+  scale_fill_brewer(palette = "Set3") +
+  theme(legend.position = "none",
+        axis.text.x = element_text(angle = 90))
+
+# Aantal meldingen per categorie
+ggplot(data, aes(x = fct_rev(fct_infreq(categoryName)))) +
+  geom_bar() +
+  labs(title = "Aantal meldingen per categorie", x = "Categorie", y = "Aantal meldingen") +
+  theme_minimal() +
+  scale_fill_brewer(palette = "Set3") +
+  theme(legend.position = "none") +
+  coord_flip()
